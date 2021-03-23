d437d51296c5195deaf818bea6db3f30e62e4b61
Joe Kessler <copperbotte@gmail.com>
2021-03-23 04:25:50 -0700

    Implemented nelder-mead optimization, fixed some bugs

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ import random
 def loadData(src="XDGUSD.csv", timescale=60):
     data = None
     
-    with open("XDGUSD.csv", 'r') as o:
+    with open(src, 'r') as o:
         text = o.read()
         lines = text.split('\n')[:-1] #exclude empty line at the end
         data = [list(map(float, L.split(','))) for L in lines]
@@ -177,6 +177,12 @@ def plots(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7):
     
     plt.show()
 
+def computeMacdProfit(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7):
+    times, prices, _,_,_,_, macd = compute_Macd(data, macdFast=macdFast, macdSlow=macdSlow, macdLag=macdLag)
+    _,_,c_pct,_ = find_profit(times, prices, macd)
+    return c_pct[-1]
+    
+
 def rndwlk(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7, sigma=0.1):
     def invgauss(sig=sigma):
         rpos = sig*np.sqrt(-np.log(1.0-random.random()))
@@ -190,9 +196,7 @@ def rndwlk(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7, si
     l_mdl = np.log(macdLag)
 
     #compute initial value
-    times, prices, _,_,_,_, macd = compute_Macd(data, macdFast=macdFast, macdSlow=macdSlow, macdLag=macdLag)
-    _,_,c_pct,_ = find_profit(times, prices, macd)
-    profit = c_pct[-1]
+    profit = computeMacdProfit(data, macdFast=macdFast, macdSlow=macdSlow, macdLag=macdLag)
 
     print("macdFast="+str(np.exp(l_mdf))+',', "macdSlow="+str(np.exp(l_mds))+',', "macdLag="+str(np.exp(l_mdl)), "profit:", profit)
 
@@ -203,9 +207,7 @@ def rndwlk(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7, si
         l_mdl_test = l_mdl + invgauss(sigma)
 
         #compute test result
-        times, prices, _,_,_,_, macd = compute_Macd(data, macdFast=np.exp(l_mdf_test), macdSlow=np.exp(l_mds_test), macdLag=np.exp(l_mdl_test))
-        _,_,c_pct,_ = find_profit(times, prices, macd)
-        profit_test = c_pct[-1]
+        profit_test = computeMacdProfit(data, macdFast=np.exp(l_mdf_test), macdSlow=np.exp(l_mds_test), macdLag=np.exp(l_mdl_test))
 
         #replace if better
         if profit < profit_test:
@@ -215,7 +217,69 @@ def rndwlk(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7, si
             profit = profit_test
             print("macdFast="+str(np.exp(l_mdf))+',', "macdSlow="+str(np.exp(l_mds))+',', "macdLag="+str(np.exp(l_mdl)), "profit:", profit)
     
+def nelderMead(data, macdFast=3600*24*7, macdSlow=3600*24*7*4, macdLag=3600*24*7):
+    #nelder-mead optimization, starting with an orthogonal set of vertices, log(param) + 1 per parameter
+    #walks are performed in log space
+    l_mdf = np.log(macdFast)
+    l_mds = np.log(macdSlow)
+    l_mdl = np.log(macdLag)
+
+    #build simplex
+    verts =     [[0, l_mdf,   l_mds,   l_mdl]]
+    verts.append([0, l_mdf+1, l_mds,   l_mdl])
+    verts.append([0, l_mdf,   l_mds+1, l_mdl])
+    verts.append([0, l_mdf,   l_mds,   l_mdl+1])
     
+    #find profits in simplex
+    for v in range(len(verts)):
+        verts[v][0] = computeMacdProfit(data, macdFast=np.exp(verts[v][1]), macdSlow=np.exp(verts[v][2]), macdLag=np.exp(verts[v][3]))
+        print("macdFast="+str(np.exp(verts[v][1]))+',', "macdSlow="+str(np.exp(verts[v][2]))+',', "macdLag="+str(np.exp(verts[v][3])), "profit:", verts[v][0])
+
+    bestProfit = 0.0
+    
+    #nelder-mead algorithm:
+    #find worst performing vertex
+    #reflect it through the centroid of all the other vertices
+    #sort the new vertex with the other two
+    #if its between the other two, no further transforms are needed
+    #if its the worst performer, shrink the vertex through the centroid
+    #if its the best performer, expand the vertex through the centroid
+    while True:
+        #find worst performing vertex
+        verts.sort(key=lambda x: x[0])
+        bP = verts[-1][0]
+        if bestProfit < bP:
+            bestProfit = bP
+            print("macdFast="+str(np.exp(verts[v][1]))+',', "macdSlow="+str(np.exp(verts[v][2]))+',', "macdLag="+str(np.exp(verts[v][3])), "profit:", verts[v][0])
+            
+        #find simplex of all other vertices
+        simp = np.array([0.0,0.0,0.0,0.0])
+        for i in range(1, len(verts)):
+            simp += np.array(verts[i]) / (len(verts) - 1)
+
+        #adjust vertex
+        delta = simp - np.array(verts[0])
+        verts[0] = (simp + delta).tolist()
+        verts[0][0] = computeMacdProfit(data, macdFast=np.exp(verts[0][1]), macdSlow=np.exp(verts[0][2]), macdLag=np.exp(verts[0][3]))
+
+        #compare to extremes
+        ext = [[verts[0][0], 'N'], [verts[1][0], 'S'], [verts[-1][0], 'L']]
+        ext.sort(key=lambda x: x[0])
+
+        #if N is in the middle: use new simplex
+        if ext[1][1] == 'N':
+            #print("macdFast="+str(np.exp(verts[v][1]))+',', "macdSlow="+str(np.exp(verts[v][2]))+',', "macdLag="+str(np.exp(verts[v][3])), "profit:", verts[v][0])
+            continue
+
+        #if N is the smallest, shrink new vertex
+        offset = 1.0
+        if ext[0][1] == 'N':
+            offset = -0.5 
+        else: #grow to new vertex
+            offset = 2.0
+
+        verts[0] = (simp + offset*delta).tolist()
+        #print("macdFast="+str(np.exp(verts[v][1]))+',', "macdSlow="+str(np.exp(verts[v][2]))+',', "macdLag="+str(np.exp(verts[v][3])), "profit:", verts[v][0])
 
 data = None
 
